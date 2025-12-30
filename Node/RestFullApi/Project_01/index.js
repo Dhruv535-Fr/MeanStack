1b2feecb1d44f256920cafa16535d66cf3f14fe0
Dhruv535-Fr <parmardhruv498@gmail.com>
2025-12-30 12:28:16 +0530

    Add RESTful API project with mock data

---
+++ Node/RestFullApi/Project_01/index.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const users = require('./MOCK_DATA.json');
+const fs = require("fs");
+
+const app = express();
+const PORT = 8000;
+
+
+//middleware - to identify post request data and convert it into js object...
+app.use(express.urlencoded({extended : false}));
+//(SSR)--> sending HTML 
+
+app.get("/users",(req,res)=>{
+    const html = `
+        <ul>
+            ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+        </ul>
+    `;
+    return res.send(html);
+});
+
+//RestAPI -> Sending json format..
+
+app.get("/api/users",(req,res)=>{ //grtting data of all user..
+    return res.json(users); 
+});
+
+// app.get("/api/users/:id",(req,res)=>{ //id dynamic variable..
+//     const id = Number(req.params.id);
+//     const user = users.find((user) => user.id === id);
+//     return res.json(user);
+// });
+
+//Writting same route multiple.. instead of that we can mearge all togather...
+
+app.route("/api/users/:id")
+
+.get((req,res)=>{ //id dynamic variable..
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+})
+.patch((req,res)=>{
+    const body = req.body;
+    const id = Number(req.params.id);
+    const userIndex = users.findIndex((user) => user.id === id);
+
+    if(userIndex === -1) {
+        return res.json({ "Status": "Error", "message": "User not found" });
+    }
+
+    users[userIndex] = { ...users[userIndex], ...body };
+
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+        return res.json({ "Status": "Success", "user": users[userIndex] });
+    });
+})
+.delete((req,res)=>{
+    const id = Number(req.params.id);
+    const userIndex = users.findIndex((user) => user.id === id);
+    
+    if(userIndex === -1) {
+        return res.status(404).json({ "Status": "Error", "message": "User not found" });
+    }
+    
+    // Remove user from array
+    users.splice(userIndex, 1);
+    
+    // Save to file
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+        return res.json({ "Status": "Success", "message": "User deleted" });
+    });
+
+});
+
+app.post("/api/users",(req,res)=>{ 
+    const body = req.body;
+    console.log(body);
+    users.push({...body,id : users.length + 1});
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data) =>{
+        return res.json({ "Status" : "Success" , id : users.length})
+    });
+});
+
+
+app.listen(PORT,() => console.log(`Server has Started at Port : ${PORT}!`));
